785e843e91bbfb745d82865bf88dd40138e88375
unknown <lamanris@gmail.com>
2022-06-29 21:47:47 +0600

    adaptive 1/2

--- js/sliders.js
+++ js/sliders.js
@@ -1,6 +1,7 @@
 const swiper = new Swiper('.slider-banner__swiper', {
     slidesPerView: 1,
     loop: true,
+    spaceBetween: 30,
     pagination: {
         el: '.slider-banner .swiper-pagination',
         clickable: true
