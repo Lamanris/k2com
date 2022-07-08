db4d98c462bf8bcb60e4d2f49c7dd421f5c56d10
unknown <lamanris@gmail.com>
2022-07-09 00:18:51 +0600

    products page spares 1/2

--- js/sliders.js
+++ js/sliders.js
@@ -8,7 +8,7 @@ const swiper = new Swiper('.slider-banner__swiper', {
     },
 });
 
-var productSwiperThumbnails = new Swiper(".product__slider-thumbnails .swiper", {
+const productSwiperThumbnails = new Swiper(".product__slider-thumbnails .swiper", {
     loop: true,
     spaceBetween: 20,
     slidesPerView: 3,
@@ -18,11 +18,21 @@ var productSwiperThumbnails = new Swiper(".product__slider-thumbnails .swiper", 
         prevEl: ".swiper-button-prev",
     },
 });
-var productSwiper = new Swiper(".product__slider-main .swiper", {
+const productSwiperWithThumbs = new Swiper(".product__slider-with-thumbs .swiper", {
     loop: true,
     spaceBetween: 10,
     grabCursor: true,
     thumbs: {
         swiper: productSwiperThumbnails,
+    },
+});
+
+const productSwiperOneSlider = new Swiper(".product__slider-one-slider .swiper", {
+    loop: true,
+    spaceBetween: 10,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".product__slider-one-slider .swiper-button-next",
+        prevEl: ".product__slider-one-slider .swiper-button-prev",
     },
 });
